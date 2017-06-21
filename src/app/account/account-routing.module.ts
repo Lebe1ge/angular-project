18d2735650650a574493aa173f02f3c42761e70d
Nicolas Souffleur <souffleur.nicolas@gmail.com>
2017-06-21 18:36:19 +0200

    Add My Series component

--- src/app/account/account-routing.module.ts
+++ src/app/account/account-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ProfileComponent } from './profile/profile.component';
+import { MySeriesComponent } from './my-series/my-series.component';
 
 const routes: Routes = [
     { path: 'my-profile', component: ProfileComponent },
+    { path: 'my-series', component: MySeriesComponent },
 ];
 
 @NgModule({
