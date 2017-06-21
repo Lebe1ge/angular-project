362e6d0f55c97a8cbd6aeeaad7f12689c275b020
bobby <jeremy.felicite@gmail.com>
2017-06-21 18:05:07 +0200

    add method add/remove/get for localStorage service

--- src/app/data-storage.service.ts
+++ src/app/data-storage.service.ts
@@ -1,33 +1,54 @@
 import { Injectable } from '@angular/core';
 
+const SUCCESS: string = "success";
+const ERROR: string = "error";
+
 @Injectable()
 export class DataStorageService {
 
   // Rajoute de la données dans le local storage
-  addData(entity: any): void {
+  addData(entity: any): string {
+
+    let nameClass: string = entity.constructor.name;
 
     if(entity.hasOwnProperty('id') === false){
-      throw new Exception('Attributes id doesnt exists on object entity');
+      throw new Error('Attributes id doesnt exists on object entity');
     }
     
-    let nameClass: string = entity.constructor.name;
     let allData: Array<any> = Object.keys(entity).filter(function(item) {return item !== 'id';}).map(function(key){
       return [key, entity[key]];
     });
 
     localStorage.setItem(nameClass + '_' + entity['id'], JSON.stringify(allData));
+    return SUCCESS;
 
   }
 
   // Remove les données stockées dans le local storage
-  removeData(entity: any): void {
+  removeData(entity: any): string {
+
     let nameClass: string = entity.constructor.name;
+
+    if(entity.hasOwnProperty('id') === false){
+      throw new Error('Attributes id doesnt exists on object entity');
+    }
+
     localStorage.removeItem(nameClass + '_' + entity['id']);
+    return SUCCESS;
   }
 
   // Récupère les données stockées dans le local storage
-  getData(entity: any): void {
+  getData(entity: any): any {
+    
     let nameClass: string = entity.constructor.name;
+    let data: object;
+
+    if(entity.hasOwnProperty('id') === false){
+      throw new Error('Attributes id doesnt exists on object entity');
+    }
+
+    data = JSON.parse(localStorage.getItem(nameClass + '_' + entity['id']));
+    return data;
   }
 
 }
