35ee0d61a4f9aa6225e3dc97f4f3a69bba50aceb
Juicy <julien.decoen@gmail.com>
2017-06-21 16:21:00 +0200

    fix providers

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { SharedRoutingModule } from './shared-routing.module';
+
 import { SerieComponent } from './serie/serie.component';
+
+import { SerieService } from './serie/serie.service';
 
 @NgModule({
   imports: [
     CommonModule,
     SharedRoutingModule
   ],
-  declarations: [SerieComponent]
+  declarations: [SerieComponent],
+  providers: [
+    SerieService
+  ],
 })
 export class SharedModule { }
