92882a1e8fe68d71aedd6e10e911a5381b46cd13
Juicy <julien.decoen@gmail.com>
2017-06-22 15:21:47 +0200

    add getSerie.service

--- src/app/shared/serie/serie.service.ts
+++ src/app/shared/serie/serie.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { Serie } from '../../entity/serie';
 import { SERIES } from '../../entity/mock-serie';
 import { Http, Response, RequestOptions, Headers } from '@angular/http';
-import { Observable } from "rxjs";
 import { BETA_SERIES_CONFIG } from '../shared-variables';
 
 
@@ -11,17 +10,18 @@ export class SerieService {
 
     link = 'https://api.betaseries.com/search/all?v=2.4&query=';
     http: Http;
+    serie: Serie = new Serie;
     series = [];
     options = new RequestOptions({
         headers: new Headers({
             'Accept': 'application/json',
-            'X-BetaSeries-Key': BETA_SERIES_CONFIG.apiKey
+            'X-BetaSeries-Key': BETA_SERIES_CONFIG.apiKey,
         })
     });
 
 
   searchSeries(searchTerm): Promise<Serie[]> {
-      var apiLink = this.link + searchTerm;
+      const apiLink = this.link + searchTerm;
       this.http.request(apiLink, this.options) .subscribe((res: Response) => {
           this.series = res.json().data;
       });
@@ -31,6 +31,15 @@ export class SerieService {
 
   getSeries(): Promise<Serie[]> {
     return Promise.resolve(SERIES);
+  }
+
+  getSerie(serieId): Promise<Serie> {
+
+    this.http.get('https://api.betaseries.com/shows/display?id=' + serieId, this.options)
+    .subscribe((res: Response) => {
+        this.serie = res.json().show;
+    });
+    return Promise.resolve(this.serie);
   }
 
   constructor (http: Http) {
