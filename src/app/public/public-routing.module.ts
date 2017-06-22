3a653065c9615267b352271ea309dbfe0034ae38
Juicy <julien.decoen@gmail.com>
2017-06-22 17:12:51 +0200

    add getSerie

--- src/app/public/public-routing.module.ts
+++ src/app/public/public-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { SearchComponent } from './search/search.component';
+import { DisplaySerieComponent } from './display-serie/display-serie.component';
 
 const routes: Routes = [
   {
@@ -12,7 +13,7 @@ const routes: Routes = [
     component: SearchComponent,
   },
   { path: 'serie/:serieId',
-    component: SearchComponent,
+    component: DisplaySerieComponent,
   }
 ];
 
--- src/app/shared/serie/serie.service.ts
+++ src/app/shared/serie/serie.service.ts
@@ -37,12 +37,32 @@ export class SerieService {
     return Promise.resolve(SERIES);
   }
 
-  getSerie(serieId): Promise<Serie> {
-    this._http.get('https://api.betaseries.com/shows/display?id=' + serieId, this.options)
-    .subscribe((res: Response) => {
-        this.serie = res.json().show;
-    });
-    return Promise.resolve(this.serie);
+  // getSerie(serieId): Promise<Serie> {
+  //   this._http.get('https://api.betaseries.com/shows/display?id=' + serieId, this.options)
+  //   .subscribe((res: Response) => {
+  //       this.serie = res.json().show;
+  //   });
+  //   return Promise.resolve(this.serie);
+  // }
+
+  getSerie(serieId): Observable<Serie> {
+    return this._http.get('https://api.betaseries.com/shows/display?id=' + serieId, this.options)
+                    .map((res: Response) => res.json().show)
+                    .catch(this.handleError);
+  }
+
+  private handleError (error: Response | any) {
+    // In a real world app, you might use a remote logging infrastructure
+    let errMsg: string;
+    if (error instanceof Response) {
+      const body = error.json() || '';
+      const err = body.error || JSON.stringify(body);
+      errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+    } else {
+      errMsg = error.message ? error.message : error.toString();
+    }
+    console.error(errMsg);
+    return Observable.throw(errMsg);
   }
 
   constructor (private _http: Http) {}
