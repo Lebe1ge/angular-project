aa57c20d3b1c09939af81a509aa7a44e470f4693
Juicy <julien.decoen@gmail.com>
2017-06-23 14:26:07 +0200

    remove mock

--- src/app/shared/serie/serie.component.ts
+++ src/app/shared/serie/serie.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { SerieService } from './serie.service';
 import { DataStorageService } from '../../data-storage.service';
 import { ActivatedRoute, Params } from '@angular/router';
-import { SERIES } from '../../entity/mock-serie';
 
 const nameService = 'serie';
 
