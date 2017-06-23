4b53774e1f718b1109a100d4b748a1599aaa5249
antonio <a.delelis94@gmail.com>
2017-06-23 16:46:56 +0200

    Add goBack button to episode Page 2.

--- src/app/public/episode/display-episode/display-episode.component.ts
+++ src/app/public/episode/display-episode/display-episode.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit, Input } from '@angular/core';
-import {Router, ActivatedRoute, Params} from '@angular/router';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 import { Episode } from '../../../entity/episode';
 import { AuthService } from '../../../shared/auth/auth.service';
 import { EpisodeService } from '../episode.service';
 import { DataStorageService } from '../../../data-storage.service';
+import { Location } from '@angular/common';
 
 const KEY_STORAGE_EPISODES = 'MyEpisodes';
 
@@ -14,21 +15,24 @@ const KEY_STORAGE_EPISODES = 'MyEpisodes';
 })
 export class DisplayEpisodeComponent implements OnInit {
 
+  // Attributes
   episode: Episode;
   errorMessage: String;
   keyStorage: string;
-
   @Input() episodeInput: Episode;
+
+
+  // Methods
 
   /**
    * Ajoute un épisode dans ceux qui sont vus
-   * @param id 
+   * @param id
    */
   addToSeen(episode: Episode): void {
 
     if( !this.auth.isAuthenticated ) {
       this.router.navigate(['/']);
-    }  
+    }
     episode.seen = true;
     this.DataStorageService.setKeyStorage(this.keyStorage);
     this.DataStorageService.add(episode.id);
@@ -36,47 +40,54 @@ export class DisplayEpisodeComponent implements OnInit {
 
   /**
    * Check si l'épisode a déjà été vu
-   * @param id 
+   * @param id
    */
   isSeen(id: number): void {
     if( !this.auth.isAuthenticated ) {
       this.router.navigate(['/']);
     }
     this.DataStorageService.setKeyStorage(this.keyStorage);
-    this.DataStorageService.getById(id);  
+    this.DataStorageService.getById(id);
   }
 
   /**
    * Supprime l'épisode des épisodes vus
-   * @param id 
+   * @param id
    */
   removeSeen(episode: Episode): void {
     if( !this.auth.isAuthenticated ) {
-      this.router.navigate(['/']);  
+      this.router.navigate(['/']);
     }
     episode.seen = false;
     this.DataStorageService.setKeyStorage(this.keyStorage);
     this.DataStorageService.removeData(episode.id);
   }
 
+  getSerie(episodeId) {
+    this.episodeService.getEpisode(episodeId)
+      .subscribe(
+        episode => this.episode = episode,
+        error =>  this.errorMessage = <any>error);
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+
+  // Lifecycle
   constructor(
-  public auth: AuthService, 
-  private activatedRoute: ActivatedRoute, 
-  private episodeService: EpisodeService,
-  private DataStorageService: DataStorageService,
-  private router: Router) { };
+    public auth: AuthService,
+    private activatedRoute: ActivatedRoute,
+    private episodeService: EpisodeService,
+    private DataStorageService: DataStorageService,
+    private router: Router,
+    private location: Location
+  ) { };
 
   ngOnInit() {
     this.activatedRoute.params.subscribe((params: Params) => {
       this.getSerie(params['episodeId']);
       this.keyStorage = KEY_STORAGE_EPISODES;
     });
-  }
-
-  getSerie(episodeId) {
-    this.episodeService.getEpisode(episodeId)
-    .subscribe(
-      episode => this.episode = episode,
-      error =>  this.errorMessage = <any>error);
   }
 }
